1642f7feab3e0b1e232f037aa9528698994dd253
haithanhphan1603 <haithanhphan163@gmail.com>
2024-01-11 14:40:09 +0700

    Add server and client projects

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,14 +1,13 @@
 <template>
-  <div class="flex flex-col h-screen dark:bg-slate-800 dark:text-white">
-    <the-header></the-header>
-    <chat-box></chat-box>
-  </div>
+  <Suspense>
+    <div class="flex flex-col h-screen dark:bg-slate-800 dark:text-white">
+      <the-header></the-header>
+      <chat-box></chat-box>
+    </div>
+  </Suspense>
 </template>
 
 <script setup lang="ts">
 import TheHeader from '../components/TheHeader.vue'
 import ChatBox from '../components/ChatBox.vue'
-import { useDark } from '@vueuse/core'
-
-const isDark = useDark()
 </script>
