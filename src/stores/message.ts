c4bde52b7ccd9adda2beedfd3d315b8007e57887
haithanhphan1603 <haithanhphan163@gmail.com>
2024-08-28 10:12:19 +0700

    update setting config

--- src/stores/message.ts
+++ src/stores/message.ts
@@ -13,7 +13,7 @@ export const useMessageStore = defineStore('message', () => {
     },
     {
       content:
-        "Hey there! Meet MiracleGPT, your brilliant friend in the digital realm. This clever conversationalist is powered by OpenAI's GPT-3.5, making it a wizard in understanding and generating natural language. It's your go-to buddy for all kinds of discussions—whether you're curious about facts, looking for creative inspiration, or just in the mood for a good chat. MiracleGPT is here to make our conversations smart, engaging, and maybe even a little bit fun. So, ready to dive into the world of information and banter with our digital sidekick? Let's get chatting!",
+        "Hey there! Meet MiracleGPT, your brilliant friend in the digital realm. This clever conversationalist is powered by OpenAI's GPT-4.0, making it a wizard in understanding and generating natural language. It's your go-to buddy for all kinds of discussions—whether you're curious about facts, looking for creative inspiration, or just in the mood for a good chat. MiracleGPT is here to make our conversations smart, engaging, and maybe even a little bit fun. So, ready to dive into the world of information and banter with our digital sidekick? Let's get chatting!",
       role: 'assistant'
     }
   ])
