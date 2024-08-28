02d76a0de07ace7375b0460defb32db8e86571e6
haithanhphan1603 <haithanhphan163@gmail.com>
2024-08-28 11:00:23 +0700

    remove library

--- src/lib/gpts.ts
+++ src/lib/gpts.ts
@@ -1,17 +1,27 @@
 import type { Message } from '../types/types'
-import OpenAI from 'openai'
 
 export async function useGpts(messageList: Message[], apiKey: string) {
   try {
-    const openai = new OpenAI({ apiKey, dangerouslyAllowBrowser: true })
-    const completion = await openai.chat.completions.create({
-      model: 'gpt-3.5-turbo',
-      messages: messageList
+    const response = await fetch('https://api.openai.com/v1/chat/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${apiKey}`
+      },
+      body: JSON.stringify({
+        model: 'gpt-3.5-turbo',
+        messages: messageList
+      })
     })
-    return completion.choices[0].message
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+
+    const data = await response.json()
+    return data
   } catch (error) {
     console.error('Failed to fetch bot reply')
-    alert(`Error : ${error}`)
     throw error
   }
 }
